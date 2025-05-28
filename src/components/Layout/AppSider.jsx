0c02832e04d96eef730f416553856ab46df9a3a8
AndrDev <dannikovandrej@gmail.com>
2025-05-28 15:35:51 +0300

    two commit

--- src/components/Layout/AppSider.jsx
+++ src/components/Layout/AppSider.jsx
@@ -1,6 +1,8 @@
-import { Layout, Card, Statistic, List, Typography } from "antd";
+import { Layout, Card, Statistic, List, Typography, Spin } from "antd";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
+import { fakeFetchCrypto, fetchAssets } from "../../api";
+import { percentDifference } from "../../utils";
 
 const siderStyle = {
   padding: "1rem",
@@ -21,18 +23,35 @@ export default function AppSider() {
 
   useEffect(() => {
     async function preload() {
+      setLoading(true);
       const { result } = await fakeFetchCrypto();
-      const assets = await fetchAssests();
+      const assets = await fetchAssets();
 
-      setAssets(assets);
+      setAssets(
+        assets.map((asset) => {
+          const coin = result.find((c) => c.id === asset.id);
+          return {
+            grow: asset.price < coin.price,
+            growPercent: percentDifference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            ...asset,
+          };
+        })
+      );
       setCrypto(result);
       setLoading(false);
     }
-  }, {});
+    preload();
+  }, []);
+
+  if (loading) {
+    return <Spin fullscreen />;
+  }
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-      {" "}
-      <Card style={{ marginBottom: "1rem" }}>
+      {assets.map((asset)=>(
+         <Card style={{ marginBottom: "1rem" }}>
         <Statistic
           title="Active"
           value={11.28}
@@ -50,6 +69,10 @@ export default function AppSider() {
             </List.Item>
           )}
         />
+      </Card>
+      )}
+   
+      {/* <Card>
         <Statistic
           title="Idle"
           value={9.3}
@@ -58,7 +81,7 @@ export default function AppSider() {
           prefix={<ArrowDownOutlined />}
           suffix="%"
         />
-      </Card>
+      </Card> */}
     </Layout.Sider>
-  );
-}
+  
+
